c6810b56753f962c7c4047ecf80533e20520deb1
BonyaSlayer <zlobasterus@gmail.com>
2022-06-27 23:01:51 +0500

    Добавлены студенты и группы студентов

---
+++ introOOP/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace introOOP;
+public class Student
+{
+    public int Id { get; set; }
+
+    public string LastName { get; set; }
+    public string Name { get; set; }   
+
+    public string Patronymic { get; set; }
+}
